07c4a666e695b2bb242b10bb9895ffd180f330c4
Sam Victor <iamvictorsam@gmail.com>
2024-03-29 04:19:46 +0100

    created

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:heart_bpm/chart.dart';
+import 'package:heart_bpm/heart_bpm.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Heart BPM Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<SensorValue> data = [];
+  List<SensorValue> bpmValues = [];
+  //  Widget chart = BPMChart(data);
+
+  bool isBPMEnabled = false;
+  Widget? dialog;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('Heart BPM Demo'),
+      ),
+      body: Column(
+        children: [
+          isBPMEnabled
+              ? dialog = HeartBPMDialog(
+                  context: context,
+                  showTextValues: true,
+                  borderRadius: 10,
+                  onRawData: (value) {
+                    setState(() {
+                      if (data.length >= 100) data.removeAt(0);
+                      data.add(value);
+                    });
+                    // chart = BPMChart(data);
+                  },
+                  onBPM: (value) => setState(() {
+                    if (bpmValues.length >= 100) bpmValues.removeAt(0);
+                    bpmValues.add(SensorValue(
+                        value: value.toDouble(), time: DateTime.now()));
+                  }),
+                  // sampleDelay: 1000 ~/ 20,
+                  // child: Container(
+                  //   height: 50,
+                  //   width: 100,
+                  //   child: BPMChart(data),
+                  // ),
+                )
+              : const SizedBox(),
+          isBPMEnabled && data.isNotEmpty
+              ? Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  height: 180,
+                  child: BPMChart(data),
+                )
+              : const SizedBox(),
+          isBPMEnabled && bpmValues.isNotEmpty
+              ? Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  constraints: const BoxConstraints.expand(height: 180),
+                  child: BPMChart(bpmValues),
+                )
+              : const SizedBox(),
+          Center(
+            child: ElevatedButton.icon(
+              icon: const Icon(Icons.favorite_rounded),
+              label: Text(isBPMEnabled ? "Stop measurement" : "Measure BPM"),
+              onPressed: () => setState(() {
+                if (isBPMEnabled) {
+                  isBPMEnabled = false;
+                  // dialog.
+                } else {
+                  isBPMEnabled = true;
+                }
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
